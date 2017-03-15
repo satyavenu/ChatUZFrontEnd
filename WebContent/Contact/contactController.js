e7bd5868278dd6a1af7371dcc2ecbbf56edc4322
Srujan <Srujan@Srujan-PC>
2017-03-15 15:39:44 +0530

    frentend

---
+++ WebContent/Contact/contactController.js
@@ -0,0 +1,4 @@
+app.controller('contactController', function($scope) {
+		console.log("contact controller")
+		$scope.message = 'Contact us! JK. This is just a demo.';
+	});
---
+++ WebContent/about/aboutController.js
@@ -0,0 +1,4 @@
+app.controller('aboutController', function($scope) {
+		console.log("About controller")
+		$scope.message = 'Look! I am an about page.';
+	});
